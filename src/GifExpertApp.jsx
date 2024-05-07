b8703b305fb4dc192f8c3d7edd2192f259b7a62b
Luis Gonzalez <lgonzalez@talana.com>
2024-05-07 17:52:08 -0500

    add more category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Samurai X", "Dragon Ball"]);
-  // console.log(categories);
+
+  const onAddCategory = () => {
+    setCategories([...categories, "HunterXHunter"]);
+  };
+
   return (
     <>
       <h1>GifExpertApp</h1>
-
+      <button onClick={onAddCategory}>Agregar</button>
       <ol>
         {categories.map((category) => (
           <li key={category}>{category}</li>
