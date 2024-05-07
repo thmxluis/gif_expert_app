8f7903f350767e47a834c9d3a97881423c839fa3
Luis Gonzalez <lgonzalez@talana.com>
2024-05-07 17:42:19 -0500

    listado de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Samurai X", "Dragon Ball"]);
+  // console.log(categories);
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
   );
 };
