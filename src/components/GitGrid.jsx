f0c448f72f68465445eee8a4c81d439586a3e861
Luis Gonzalez <lgonzalez@talana.com>
2024-05-13 17:47:22 -0500

    Separar Fetch

--- src/components/GitGrid.jsx
+++ src/components/GitGrid.jsx
@@ -1,16 +1,5 @@
-const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=7nAy1xfk5t1An9zcEeomSiXyTsh8jsQR`;
-  const response = await fetch(url);
-  const { data } = await response.json();
-  const gifs = data.map((gif) => {
-    return {
-      id: gif.id,
-      title: gif.title,
-      url: gif.images.downsized_medium.url,
-    };
-  });
-  console.log(gifs);
-};
+import { getGifs } from "../helpers/getGifs";
+
 
 export const GiftGrid = ({ category }) => {
   getGifs(category);
---
+++ src/helpers/getGifs.jsx
@@ -0,0 +1,13 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=7nAy1xfk5t1An9zcEeomSiXyTsh8jsQR`;
+  const response = await fetch(url);
+  const { data } = await response.json();
+  const gifs = data.map((gif) => {
+    return {
+      id: gif.id,
+      title: gif.title,
+      url: gif.images.downsized_medium.url,
+    };
+  });
+  console.log(gifs);
+};
