5e005ffde508a8b2d6f2d8834638fbcad735202b
Luis Gonzalez <lgonzalez@talana.com>
2024-05-19 11:40:35 -0500

    + useEffect

--- src/components/GitGrid.jsx
+++ src/components/GitGrid.jsx
@@ -1,12 +1,18 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
-
 export const GiftGrid = ({ category }) => {
-  getGifs(category);
+  const [counter, setCounter] = useState(10);
+
+  useEffect(() => {
+    getGifs(category);
+  }, []);
+
   return (
     <>
       <h3>{category}</h3>
-      <p>Hola mundo</p>
+      <h5>{counter}</h5>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
     </>
   );
 };
--- src/helpers/getGifs.jsx
+++ src/helpers/getGifs.jsx
@@ -2,12 +2,12 @@ export const getGifs = async (category) => {
   const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=7nAy1xfk5t1An9zcEeomSiXyTsh8jsQR`;
   const response = await fetch(url);
   const { data } = await response.json();
-  const gifs = data.map((gif) => {
-    return {
-      id: gif.id,
-      title: gif.title,
-      url: gif.images.downsized_medium.url,
-    };
-  });
+  const gifs = data.map((gif) => ({
+    id: gif.id,
+    title: gif.title,
+    url: gif.images.downsized_medium.url,
+  }));
+
   console.log(gifs);
+  return gifs;
 };
