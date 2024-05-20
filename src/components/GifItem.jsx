8fb94bd6227c9c1edf2d77c49a8493aedb1f6399
Luis Gonzalez <lgonzalez@talana.com>
2024-05-20 16:35:15 -0500

    fix

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -2,7 +2,7 @@ export const GifItem = ({ title, url }) => {
   return (
     <div className="card">
       <img src={url} alt={title} />
-      <p>{title}</p>/
+      <p>{title}</p>
     </div>
   );
 };
