1aefd9b563c78bd52da7b4452941f6a59b149151
Luis Gonzalez <lgonzalez@talana.com>
2024-05-22 18:06:33 -0500

    + test al helper getGifs

---
+++ tests/helpers/getGifs.test.jsx
@@ -0,0 +1,14 @@
+import { getGifs } from "../../src/helpers/getGifs";
+
+describe("Pruebas en el archivo getGifs", () => {
+  test("Debe retornar un arreglo", async () => {
+    const gifs = await getGifs("One Punch");
+    // console.log(gifs);
+    expect(gifs.length).toBeGreaterThan(0);
+    expect(gifs[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      url: expect.any(String),
+    });
+  });
+});
